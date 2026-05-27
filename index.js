c536b96179fd78946d4d17632e97cb1bc565539b
Nicole <nicoleoliveiraa2002@gmail.com>
2026-05-27 16:03:26 -0300

    aumentar limite upload

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app  = express();
 const PORT = process.env.PORT || 3000;
 
 // ── Middlewares ────────────────────────────────────────────────────
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Sessões (em produção use connect-pg-simple apontando pro Supabase)
 app.use(session({
